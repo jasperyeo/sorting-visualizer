41f9adb86c35b87c75f48e8d509c03f409933ed0
Jasper <jasper.yeo.media@gmail.com>
2021-11-30 13:46:06 +0800

    [Feature] Added i18n and added Chinese and Bahasa translations

--- src/app/components/sorting-visualizer/sorting-visualizer.module.ts
+++ src/app/components/sorting-visualizer/sorting-visualizer.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { TranslateModule } from '@ngx-translate/core';
 
 import { SharedModule } from './../../shared/shared.module';
 
@@ -13,6 +14,7 @@ import { SortingVisualizerComponent } from './sorting-visualizer.component';
   imports: [
     CommonModule,
     FormsModule,
+    TranslateModule,
     SharedModule
   ],
   exports: [
