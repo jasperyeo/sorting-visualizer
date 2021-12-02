f2de371fee00db563e0633ad02abffd42cddcb21
Jasper <jasper.yeo.media@gmail.com>
2021-12-03 04:14:26 +0800

    [Feature] Added SEO title and meta description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -8,7 +9,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
 
-  public readonly title: string = 'sorting-visualizer';
+  public readonly title: string = 'Sorting Visualizer';
   public readonly langs: any[] = [
     { label: 'English', value: 'en' },
     { label: '中文', value: 'zh' },
@@ -16,7 +17,17 @@ export class AppComponent {
     { label: 'Bahasa', value: 'id' },
   ];
 
-  constructor(private _translateService: TranslateService) {
+  constructor(private _meta: Meta, private _title: Title, private _translateService: TranslateService) {
+    this._meta.addTags([
+      { name: 'description', content: 'Sorting visualizer that demonstrates the functionality of common sorting algorithms visually, through colors, sounds, and timings.' },
+      { name: 'author', content: 'Jasper Yeo' },
+      { name: 'keywords', content: 'sorting, angular, sort, sorting-algorithms, sorting-visualization, sorting-visualizer' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'date', content: '2021-12-01', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' }
+    ]);
+    this._title.setTitle(this.title);
+
     this._translateService.addLangs(this.langs.map(lang => lang.value));
     this._translateService.setDefaultLang('en');
     this._translateService.use('en');
