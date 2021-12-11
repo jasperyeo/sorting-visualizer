112538119a58d392b896ecb837219f0968274110
Jasper <jasper.yeo.media@gmail.com>
2021-12-12 04:33:32 +0800

    [Feature] Increased prod budget, added balloon visual style, fixed comparison dialog

--- src/app/components/sorting-visualizer/sorting-visualizer.component.ts
+++ src/app/components/sorting-visualizer/sorting-visualizer.component.ts
@@ -5,6 +5,7 @@ import { SortBarStyle, SortBarComponent } from './../../shared/models/sort-bar/s
 import { SortingVisualizerService } from './sorting-visualizer.service';
 
 import * as algorithms from './../../algorithms/index';
+import { complexityTime, complexitySpace } from './../../shared/models/complexity-time-space';
 
 @Component({
   selector: 'sorting-visualizer',
@@ -14,14 +15,17 @@ import * as algorithms from './../../algorithms/index';
 export class SortingVisualizerComponent implements OnInit {
 
   @Input('langs') public langs: any[] = [];
+  public readonly complexityTime = complexityTime;
+  public readonly complexitySpace = complexitySpace;
   public readonly audioContext: AudioContext = new AudioContext();
   public sortArray: SortBarComponent[] = [];
   public lang: string = 'en';
+  public selectedAlgorithm: any;
   public sortDelay: number = 50;
   public sortMethod: string = '';
   public sortDescription: string = '';
   public sortLink: string = '';
-  public sortStyle: string = SortBarStyle.BAR;
+  public sortStyle: string = SortBarStyle.BALLOON;
   public sortAttempts: number = 0;
   public elementCount: number = 400;
   public minValue: number = 5;
@@ -44,7 +48,8 @@ export class SortingVisualizerComponent implements OnInit {
 
   public readonly sortStyles: string[] = [
     SortBarStyle.BAR,
-    SortBarStyle.POINT
+    SortBarStyle.POINT,
+    SortBarStyle.BALLOON
   ];
   
   public sortAlgorithms: any[] = [];
@@ -58,7 +63,7 @@ export class SortingVisualizerComponent implements OnInit {
   }
 
   public ngOnInit(): void {
-    this._sortingVisualizerService.getJSON('./../../../assets/algorithms.json').then((res: any) => {
+    this._sortingVisualizerService.getJSON('./assets/algorithms.json').then((res: any) => {
       this.sortAlgorithms = res;
       this._scrapAlgorithmInformation();
     });
@@ -152,6 +157,7 @@ export class SortingVisualizerComponent implements OnInit {
         if (algo.value === mode) {
           this.sortDescription = algo.description;
           this.sortLink = algo.link;
+          this.selectedAlgorithm = algo;
         }
       });
     });
