705406933e42d2cd0a10aac677d6e2cf615e5991
Jasper <jasper.yeo.media@gmail.com>
2021-12-24 13:47:59 +0800

    [Refactor] Various refactors and fixes

--- src/app/algorithms/bitonic-sort.ts
+++ src/app/algorithms/bitonic-sort.ts
@@ -6,8 +6,11 @@ export async function bitonicSort(visualizer: SortingVisualizerComponent, array:
   const n: number = array.length;
   let k: number, j: number, l: number, i: number;
   for (k = 2; k <= n; k *= 2) {
+    if (!visualizer.sorting) return;
     for (j = k / 2; j > 0; j /= 2) {
+      if (!visualizer.sorting) return;
       for (i = 0; i < n; i++) {
+        if (!visualizer.sorting) return;
         l = i ^ j;
         if (l > i) {
           if ( ((i&k)==0) && compare(visualizer, array[i], array[l]) || ( ((i&k)!=0) && compare(visualizer, array[l], array[i])) )  {
