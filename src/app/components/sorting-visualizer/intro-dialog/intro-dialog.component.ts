1fe0af405cad038c5995159cd02b48715de353a5
Jasper Yeo <jasper.yeo.media@gmail.com>
2026-07-19 13:01:31 +0800

    refactor: intro dialog to use html dialog element

--- src/app/components/sorting-visualizer/intro-dialog/intro-dialog.component.ts
+++ src/app/components/sorting-visualizer/intro-dialog/intro-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, WritableSignal, signal, output, OutputEmitterRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, WritableSignal, signal, output, OutputEmitterRef, AfterViewInit } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
@@ -11,13 +11,18 @@ import { TranslatePipe } from '@ngx-translate/core';
   templateUrl: './intro-dialog.component.html',
   styleUrls: ['./intro-dialog.component.scss'],
 })
-export class IntroDialogComponent implements OnInit {
+export class IntroDialogComponent implements OnInit, AfterViewInit {
 
   protected readonly onDialogClose: OutputEmitterRef<boolean> = output<boolean>({ alias: 'onDialogClose' });
   protected readonly currentYear: WritableSignal<number> = signal<number>(2026);
 
   public ngOnInit(): void {
     this.currentYear.update(() => new Date().getFullYear());
+  }
+
+  public ngAfterViewInit(): void {
+    const dialog = document.getElementById('intro-dialog') as HTMLDialogElement;
+    dialog.showModal();
   }
 
   protected closeDialog(): void {
